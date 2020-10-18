4e68500f6a1842a5f14c822bde91943c40d7e904
thunderbolt181 <56231406+thunderbolt181@users.noreply.github.com>
2020-10-18 11:11:54 +0530

    .

--- SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/settings.py
@@ -165,5 +165,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'baghel.sachin47@gmail.com'
-EMAIL_HOST_PASSWORD = 'amfw sbmk icta jxbj'
+EMAIL_HOST_USER = os.environ['DEMAIL']
+EMAIL_HOST_PASSWORD = os.environ['DAPASS']
